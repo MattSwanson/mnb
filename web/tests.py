b86887d70c0c5ea2f91aad2a01ad9633e5c7b752
Matt Swanson <matt@minnesotabolt.com>
2016-06-22 14:51:18 -0400

    Full coverage.. for now.

--- web/tests.py
+++ web/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 
+from web.models import *
+
 # Create your tests here.
 
 class SimpleTests(TestCase):
@@ -13,3 +15,20 @@ class InitViewTests(TestCase):
         response = self.client.get(reverse('web:index'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "This is the beginning...")
+
+class ItemModelTests(TestCase):
+    def test_str(self):
+        item_num = 'Test123'
+        item = Item(item_number=item_num)
+        self.assertEqual(item.__str__(), item_num)
+
+
+class ItemRevisionModelTests(TestCase):
+    def test_str(self):
+        item_num = 'Test123'
+        rev_name = '01'
+        item = Item(item_number=item_num)
+        item_rev = ItemRevision(item=item, name=rev_name)
+        s = '%s %s' % (item_num, rev_name)
+        self.assertEquals(item_rev.__str__(), s) 
+        
