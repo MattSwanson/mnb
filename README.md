33c6a136a92cc1549d98ff439d1d92d13d2ad61b
Matt Swanson <matt@minnesotabolt.com>
2016-06-22 12:41:19 -0400

    Add coverage to readme and add a view and test to test the view

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # mnb
 
 [![Build Status](https://travis-ci.org/MattSwanson/mnb.svg?branch_master)](https://travis-ci.org/MattSwanson/mnb)
+[![Coverage Status](https://coveralls.io/repos/github/MattSwanson/mnb/badge.svg?branch=master)](https://coveralls.io/github/MattSwanson/mnb?branch=master)
--- web/tests.py
+++ web/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
 # Create your tests here.
 
@@ -6,3 +7,9 @@ class SimpleTests(TestCase):
     def test_variable_assignment(self):
         x = 1
         self.assertEqual(x,1)
+
+class InitViewTests(TestCase):
+    def test_index(self):
+        response = self.client.get(reverse('web:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "This is the beginning...")
