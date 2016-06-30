aeeea70da71643eb650d6b333390bffb482d9590
Matt Swanson <matt@minnesotabolt.com>
2016-06-30 18:55:55 -0400

    Remove api root view since it is redundant with DefaultRouter

--- api/views.py
+++ api/views.py
@@ -1,17 +1,12 @@
 from django.http import HttpResponse
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
-from rest_framework.decorators import api_view
 from rest_framework import viewsets
 
 from api.models import *
 from api.serializers import *
 
-@api_view(['GET'])
-def api_root(request, format=None):
-    return response({
-        'bags': reverse('bag-list', request=request, format=format),
-    })
+    
 
 class BagViewSet(viewsets.ModelViewSet):
     queryset = Bag.objects.all()
